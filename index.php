67e2c8ee1f720a4df0c0e7da31bf4b54f885784d
unknown <srue954@wit-xdvm-078.MILLARDPS.NET>
2012-12-04 11:26:55 -0600

    Added styling and markup

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php
+<html>
+<head>
+	<title>Gaming Blog</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<?php include './layout/header.php'?>
+		</header>
+		<nav>
+			<?php include './layout/nav.php'?>
+		</nav>
+		<div id="content">
+			<?php include './layout/content.php'?>
+		</div>
+		<footer>
+			<?php include './layout/footer.php'?>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ layout/nav.php
@@ -0,0 +1,11 @@
+<ul class="mainmenu">
+	<li id="first-child"><a href="./?p=public/home">Home</a></li>
+	<li><a href="./?p=public/posts">Posts</a></li>
+	<li><a href="#">Admin</a>
+		<ul  class="submenu">
+			<li><a href="./?p=admin/form_add_post">Add Post</a></li>
+			<li><a href="./?p=admin/form_edit_post">Edit Post</a></li>
+			<li><a href="./?p=admin/list_posts">List Posts</a></li>
+		</ul>
+	</li>
+</ul>
